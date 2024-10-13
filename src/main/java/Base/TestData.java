f11be9e98dd8487d207fc1d261150bdc1b7c59a1
gktemur <gk.temur@outlook.com>
2024-10-13 18:33:17 +0300

    Test automation added.

---
+++ src/main/java/Base/TestData.java
@@ -0,0 +1,8 @@
+package Base;
+
+public class TestData {
+    public String mail = "test.gamze@outlook.com";
+    public String password = "Gamzetest1";
+    public String url = "https://www.trendyol.com/giris";
+
+}
